40edf14e2b090bc25eb52d3510aff1237fc279a2
s-yamauchi07 <hand1nhand306@gmail.com>
2023-04-30 15:05:18 +0900

    新規登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'api/auth/registrations'
+    }
+  
+    namespace :auth do
+      resources :sessions, only: [:index]
+    end
+  end
 end
