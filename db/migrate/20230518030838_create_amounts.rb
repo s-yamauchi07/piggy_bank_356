570bf4b552c01b762b9a5de3a271d01f29a3de60
s-yamauchi07 <hand1nhand306@gmail.com>
2023-05-18 12:10:46 +0900

    カラムの追加

--- db/migrate/20230518030838_create_amounts.rb
+++ db/migrate/20230518030838_create_amounts.rb
@@ -1,7 +1,8 @@
 class CreateAmounts < ActiveRecord::Migration[6.0]
   def change
     create_table :amounts do |t|
-
+      t.integer :amount, null: false, default: ""
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
