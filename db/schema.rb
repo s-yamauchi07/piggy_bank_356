d371215fc178826922b47367fb31d26d142242dd
s-yamauchi07 <hand1nhand306@gmail.com>
2023-05-24 00:12:14 +0900

    savingモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_18_030838) do
+ActiveRecord::Schema.define(version: 2023_05_23_032652) do
 
   create_table "amounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "amount", null: false
+    t.integer "total_amount", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_amounts_on_user_id"
+  end
+
+  create_table "savings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "save_amount", null: false
+    t.bigint "user_id", null: false
+    t.boolean "checked", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_savings_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -46,4 +55,5 @@ ActiveRecord::Schema.define(version: 2023_05_18_030838) do
   end
 
   add_foreign_key "amounts", "users"
+  add_foreign_key "savings", "users"
 end
