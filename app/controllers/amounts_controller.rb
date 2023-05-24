efe3c82a4b550a98abb5123f2645966eb42ab251
s-yamauchi07 <hand1nhand306@gmail.com>
2023-05-25 00:20:42 +0900

    indexアクションの追加+creteアクション内の記述修正

--- app/controllers/amounts_controller.rb
+++ app/controllers/amounts_controller.rb
@@ -1,16 +1,31 @@
 class AmountsController < ApplicationController
+  def index
+    amounts = Amount.find_by(params[:user_id])
+    render json: amounts
+  end
+
   def create
+    saving = Saving.find_by(user_id: params[:user_id], save_amount: params[:save_amount])
+    if saving.present?
+      saving.destroy
+    else
+      Saving.create(saving_amount_params)
+    end
+
     amount = Amount.find_by(user_id: params[:user_id])
     if amount.present?
-      amount.update(amount_params)
+      amount.update(total_amount_params)
     else
-      Amount.create(amount_params)
+      Amount.create(total_amount_params)
     end
-    head :created
   end
 
   private
-  def amount_params
+  def total_amount_params
     params.permit(:total_amount, :user_id)
+  end
+
+  def saving_amount_params
+    params.permit(:save_amount, :checked, :user_id)
   end
 end
